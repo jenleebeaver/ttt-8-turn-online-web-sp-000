8477bbf10a3ef251b1eddd34564f8cee42c503ff
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-08-31 07:47:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,7 +24,8 @@ def valid_move?(board, index)
     end
 end
 
-def move(board, index, token == "X")
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
 
 def position_taken?(board, index)
