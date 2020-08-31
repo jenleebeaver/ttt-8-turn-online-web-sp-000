597c67918a44d0ab0681c93496a7a0c2c80ad851
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-08-31 07:41:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,9 +12,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index
+end
+
 def position_taken?(board, index)
   board[index] != " "
 end
 
-def input_to_index
-end
+
